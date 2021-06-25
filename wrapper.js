bbb225754fed2e0fbd9153d1a554dd7919561bd3
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-25 15:53:12 +0100

    Implementing put method

--- wrapper.js
+++ wrapper.js
@@ -1,16 +1,29 @@
 class FetchWrapper {
-    constructor(URL) {
-        this.URL = URL
+    constructor(BaseURL) {
+        this.BaseURL = BaseURL
     }
 
     get(endpoint) {
-        return fetch(endpoint, {
-            method : "get",
+        return fetch( this.BaseURL + endpoint, {
+            method : "get", //This method is the default method for sending request.
             headers : {
-                "Content-Type" : "Application/JSON"
+                "Content-Type" : "Application/JSON" // headers content type.
             }
         }).then(response => {
             return  response.json()
+        })
+    }
+
+    put(endpoint, content) {
+        return fetch(this.BaseURL + endpoint, {
+            method : 'put', 
+            headers : {
+                "Content-Type" : "Application/JSON"
+            },
+            body : JSON.stringify(content)
+        })
+        .then(response => {
+            return response.json()
         })
     }
 }
